85c93e58708e7fa4062b44cf362b1d0c787ebc74
BrenoRev <silvabreno462@gmail.com>
2021-10-19 09:42:41 -0300

    Criando a Barra de Menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,4 @@
 import { LoginComponent } from './login/login.component';
-import { AppComponent } from './app.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
-import { Component } from "@angular/core";
+import { Router } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
 
-  constructor(){
+  constructor(private router: Router){
 
 }
+  ngOnInit(): void {
+
+    // Verificar a token do usuário
+    if(localStorage.getItem('token') == null){
+      this.router.navigateByUrl('login')
+    }
+  }
+
+  sair(): void{
+    localStorage.clear();
+    this.router.navigateByUrl('login')
+  }
 }
